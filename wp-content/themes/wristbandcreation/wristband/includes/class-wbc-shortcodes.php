d228077b20f75ba72cbab5b4a7e6c185ac5eb666
Joy <philwebservices.programmer04@gmail.com>
2015-09-24 10:24:32 +0800

    Added shortcode class

--- wp-content/themes/wristbandcreation/wristband/includes/class-wbc-shortcodes.php
+++ wp-content/themes/wristbandcreation/wristband/includes/class-wbc-shortcodes.php
@@ -12,16 +12,14 @@ if (!class_exists('WBC_Shortcodes')) {
             add_shortcode('wc-product-price', array($this, 'render_wc_product_price'), 10, 1);
         }
 
-
         public function render_wc_product_price($atts)
         {
             extract(shortcode_atts(array(
                 'id' => '',
             ), $atts, 'wc-product-price'));
-//
-//            $post = get_post($id, OBJECT);
 
             ob_start();
+
             include_once('views/html-wc-product-price.php');
 
             return ob_get_clean();
--- wp-content/themes/wristbandcreation/wristband/includes/views/html-wc-product-price.php
+++ wp-content/themes/wristbandcreation/wristband/includes/views/html-wc-product-price.php
@@ -1,9 +1,14 @@
 <?php
+
 echo '<pre>';
+
 if (isset($GLOBALS['wbc_settings']->products->{$id})) {
     print_r($GLOBALS['wbc_settings']->products->{$id});
+
 } else {
     echo __("ID: {$id} not exists.");
 }
+
+
 
 echo '</pre>';
