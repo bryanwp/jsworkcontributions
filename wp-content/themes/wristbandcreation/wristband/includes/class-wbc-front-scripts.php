bd884e096abd2583a4e2338f2f99909f2b677585
Joy <philwebservices.programmer04@gmail.com>
2015-10-14 09:10:23 +0800

    Fixed preview container

--- wp-content/themes/wristbandcreation/wristband/includes/class-wbc-front-scripts.php
+++ wp-content/themes/wristbandcreation/wristband/includes/class-wbc-front-scripts.php
@@ -23,10 +23,7 @@ if (!class_exists('WBC_Front_Scripts')) {
             wp_register_script('jquery-iframe-transport_js', WBC_ASSETS_URL . '/js/vendor/jquery-fileupload/jquery.iframe-transport.js', array('jquery'), WBC_VERSION, true);
             wp_register_script('jquery-fileupload_js', WBC_ASSETS_URL . '/js/vendor/jquery-fileupload/jquery.fileupload.js', array('jquery'), WBC_VERSION, true);
             wp_register_script('select2_js', WBC_ASSETS_URL . '/js/vendor/select2.min.js', array('jquery'), WBC_VERSION, true);
-           // wp_register_script('material_js', WBC_ASSETS_URL . '/js/vendor/material.min.js', array('jquery'), WBC_VERSION, true);
             wp_register_script('pablo_js', WBC_ASSETS_URL . '/js/vendor/pablo.js', array('jquery'), WBC_VERSION, true);
-//            wp_register_script('jquery_ui_js', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js', array('jquery'), WBC_VERSION, true);
-//            wp_register_script('evol_colopicker_js', WBC_ASSETS_URL . '/js/vendor/colorpicker-master/evol.colorpicker.min.js', array('jquery'), WBC_VERSION, true);
             wp_register_script('mustache_js', WBC_ASSETS_URL . '/js/vendor/mustache.min.js', array('jquery'), WBC_VERSION, true);
             wp_register_script('wristband_js', WBC_ASSETS_URL . '/js/wristband.js', array('jquery'), WBC_VERSION, true);
 
@@ -40,8 +37,6 @@ if (!class_exists('WBC_Front_Scripts')) {
             wp_enqueue_script('material_js');
             wp_enqueue_script('pablo_js');
 
-//            wp_enqueue_script('jquery_ui_js');
-//            wp_enqueue_script('evol_colopicker_js');
             wp_enqueue_script('mustache_js');
             wp_enqueue_script('wristband_js');
 
@@ -56,9 +51,6 @@ if (!class_exists('WBC_Front_Scripts')) {
         public function load_styles() {
             wp_register_style('jquery-file-upload_style', WBC_ASSETS_URL . '/css/vendor/jquery-fileupload/jquery.fileupload.css', array(), WBC_VERSION);
             wp_register_style('select2_style', WBC_ASSETS_URL . '/css/vendor/select2.min.css', array(), WBC_VERSION);
-           // wp_register_style('material_style', WBC_ASSETS_URL . '/css/vendor/material.min.css', array(), WBC_VERSION);
-//            wp_register_style('jquery_ui_style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/ui-lightness/jquery-ui.css', array(), WBC_VERSION);
-//            wp_register_style('evol_colorpicker_style', WBC_ASSETS_URL . '/css/vendor/colorpicker-master/evol.colorpicker.min.css', array(), WBC_VERSION);
             wp_register_style('wristband_style', WBC_ASSETS_URL . '/css/wristband.css', array(), WBC_VERSION);
 
 
@@ -66,8 +58,6 @@ if (!class_exists('WBC_Front_Scripts')) {
             wp_enqueue_style('select2_style');
             wp_enqueue_style('material_style');
 
-//            wp_enqueue_style('jquery_ui_style');
-//            wp_enqueue_style('evol_colorpicker_style');
             wp_enqueue_style('wristband_style');
 
         }
