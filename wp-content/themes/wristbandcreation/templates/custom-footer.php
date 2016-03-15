f5c1592bdb259edbbc68860f31376907041f0307
Sheryl Lopez <sheryl.l@lightmedia.com.au>
2016-03-15 09:06:58 +0800

    custom footer

--- wp-content/themes/wristbandcreation/templates/custom-footer.php
+++ wp-content/themes/wristbandcreation/templates/custom-footer.php
@@ -33,40 +33,40 @@
 						}
 
 					?>
-						<footer class="fusion-footer-widget-area fusion-widget-area<?php echo $footer_widget_area_center_class; ?>">
+						<!-- <footer class="fusion-footer-widget-area fusion-widget-area<?php echo $footer_widget_area_center_class; ?>">
 							<div class="fusion-row">
-								<div class="fusion-columns fusion-columns-<?php echo Avada()->settings->get( 'footer_widgets_columns' ); ?> fusion-widget-area">
+								<div class="fusion-columns fusion-columns-<?php //echo Avada()->settings->get( 'footer_widgets_columns' ); ?> fusion-widget-area"> -->
 
 									<?php
 									// Check the column width based on the amount of columns chosen in Theme Options
-									$column_width = 12 / Avada()->settings->get( 'footer_widgets_columns' );
-									if( Avada()->settings->get( 'footer_widgets_columns' ) == '5' ) {
-										$column_width = 2;
-									}
+									// $column_width = 12 / Avada()->settings->get( 'footer_widgets_columns' );
+									// if( Avada()->settings->get( 'footer_widgets_columns' ) == '5' ) {
+									// 	$column_width = 2;
+									// }
 
-									// Render as many widget columns as have been chosen in Theme Options
-									for ( $i = 1; $i < 7; $i++ ) {
-										if ( Avada()->settings->get( 'footer_widgets_columns' ) >= $i ) {
-											if ( Avada()->settings->get( 'footer_widgets_columns' ) == $i ) {
-												echo sprintf( '<div class="fusion-column fusion-column-last col-lg-%s col-md-%s col-sm-%s">', $column_width, $column_width, $column_width );
-											} else {
-												echo sprintf( '<div class="fusion-column col-lg-%s col-md-%s col-sm-%s">', $column_width, $column_width, $column_width );
-											}
+									// // Render as many widget columns as have been chosen in Theme Options
+									// for ( $i = 1; $i < 7; $i++ ) {
+									// 	if ( Avada()->settings->get( 'footer_widgets_columns' ) >= $i ) {
+									// 		if ( Avada()->settings->get( 'footer_widgets_columns' ) == $i ) {
+									// 			echo sprintf( '<div class="fusion-column fusion-column-last col-lg-%s col-md-%s col-sm-%s">', $column_width, $column_width, $column_width );
+									// 		} else {
+									// 			echo sprintf( '<div class="fusion-column col-lg-%s col-md-%s col-sm-%s">', $column_width, $column_width, $column_width );
+									// 		}
 
-												if ( function_exists( 'dynamic_sidebar' ) &&
-													 dynamic_sidebar( 'avada-footer-widget-' . $i )
-												) {
-													// All is good, dynamic_sidebar() already called the rendering
-												}
-											echo '</div>';
-										}
-									}								
+									// 			if ( function_exists( 'dynamic_sidebar' ) &&
+									// 				 dynamic_sidebar( 'avada-footer-widget-' . $i )
+									// 			) {
+									// 				// All is good, dynamic_sidebar() already called the rendering
+									// 			}
+									// 		echo '</div>';
+									// 	}
+									// }								
 									?>
 
-									<div class="fusion-clearfix"></div>
-								</div> <!-- fusion-columns -->
-							</div> <!-- fusion-row -->
-						</footer> <!-- fusion-footer-widget-area -->
+									<!-- <div class="fusion-clearfix"></div> -->
+								<!-- </div> --> <!-- fusion-columns -->
+							<!-- </div> --> <!-- fusion-row -->
+						<!-- </footer> --> <!-- fusion-footer-widget-area -->
 					<?php
 					} // end footer wigets check
 
