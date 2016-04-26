98e17384b8b50a15f66ed0ea1e5f33332b4dbd5c
Sheryl Lopez <sheryl.l@lightmedia.com.au>
2016-04-26 17:12:45 +0800

    s

--- wp-content/themes/wristbandcreation/templates/custom-header.php
+++ wp-content/themes/wristbandcreation/templates/custom-header.php
@@ -155,7 +155,7 @@ if ( 'modern' == Avada()->settings->get( 'mobile_menu_design' ) ) {
 			<div class="log-in-logo">
 				<a class="fusion-logo-link" href="<?php echo home_url(); ?>">
 					<?php $logo_url = Avada_Sanitize::get_url_with_correct_scheme( Avada()->settings->get( 'logo' ) );?>
-					<img src="<?php echo $logo_url['url']; ?>" width="<?php echo $logo_size['width']; ?>" height="<?php echo $logo_size['height']; ?>" alt="<?php bloginfo( 'name' ); ?>" class="fusion-logo-1x fusion-standard-logo" />
+					<img src="<?php echo $logo_url; ?>" width="<?php echo $logo_size['width']; ?>" height="<?php echo $logo_size['height']; ?>" alt="<?php bloginfo( 'name' ); ?>" class="fusion-logo-1x fusion-standard-logo" />
 				</a>
 			</div>
 			<div class="welcome">
